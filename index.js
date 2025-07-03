7869d547b783d4b92d2b79330aaf1f84f37c48af
Hinckley00 <nwaelelehinckley@gmail.com>
2025-07-03 12:10:42 +0100

    database

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import Product from "./models/product.model.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+mongoose
+  .connect(
+    "mongodb+srv://nwaelelehinckley:yelkcniH12345@cluster0.rn8nl5j.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
